2569be46fd3f68b4c8705ee4bec50446c5e7ce19
carlio <git@carlcrowder.com>
2013-12-25 12:26:38 +0000

    Initial project scaffolding

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+from pysimilar import __pkginfo__
+
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+_short_description = "Dodgy: Searches for dodgy looking lines in Python code"
+
+_install_requires = [
+    'argparse==1.2.1',
+]
+
+_classifiers = (
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Unix',
+    'Topic :: Software Development :: Quality Assurance',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+)
+
+setup(
+    name='dodgy',
+    url='https://github.com/landscapeio/dodgy',
+    author='landscape.io',
+    author_email='code@landscape.io',
+    description=_short_description,
+    install_requires=_install_requires,
+    scripts=['bin/dodgy'],
+    version=__pkginfo__.get_version(),
+    packages=_packages,
+    license='MIT',
+    keywords='check for suspicious code',
+    classifiers=_classifiers
+)
