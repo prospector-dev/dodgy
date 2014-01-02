18c9ea44b32acb8cd077003e8b8d7dc3c28aea2f
carlio <git@carlcrowder.com>
2014-01-02 16:31:35 +0100

    The AWS check now also checks for "AWS" near by, as otherwise it could pick up non-AWS passwords

--- dodgy/__pkginfo__.py
+++ dodgy/__pkginfo__.py
@@ -1,5 +1,5 @@
 
-VERSION = (0, 1)
+VERSION = (0, 1, 1)
 
 
 def get_version():
--- dodgy/checks.py
+++ dodgy/checks.py
@@ -2,27 +2,38 @@ import re
 
 
 STRING_VALS = (
-    ('aws_secret_key',
-     'Amazon Web Services secret key',
-     re.compile(r'(\'|")[A-Za-z0-9\\\+]{40}(\'|")')
+    (
+        'aws_secret_key',
+        'Amazon Web Services secret key',
+        (
+            re.compile(r'(\'|")[A-Za-z0-9\\\+]{40}(\'|")'),
+            re.compile(r'(\b|_)AWS(\b|_)', re.IGNORECASE)
+        ),
+        all
     ),
 )
 
 LINE_VALS = (
-    ('diff',
-     'Possible SCM diff in code',
-     (re.compile(r'^<<<<<<< .*$'), re.compile(r'^>>>>>>> .*$'))
+    (
+        'diff',
+        'Possible SCM diff in code',
+        (
+            re.compile(r'^<<<<<<< .*$'),
+            re.compile(r'^>>>>>>> .*$')
+        ),
     ),
 )
 
 VAR_NAMES = (
-    ('password',
-     'Possible hardcoded password',
-     re.compile(r'(\b|[A-Z0-9_]*_)PASSWORD(_[A-Z0-9_]*|\b)\s*=\s(\'|")[^\'"]+(\'|")')
+    (
+        'password',
+        'Possible hardcoded password',
+        re.compile(r'(\b|[A-Z0-9_]*_)PASSWORD(_[A-Z0-9_]*|\b)\s*=\s(\'|")[^\'"]+(\'|")')
     ),
-    ('secret',
-     'Possible hardcoded secret key',
-     re.compile(r'(\b|[A-Z0-9_]*_)SECRET(_[A-Z0-9_]*|\b)\s*=\s(\'|")[^\'"]+(\'|")')
+    (
+        'secret',
+        'Possible hardcoded secret key',
+        re.compile(r'(\b|[A-Z0-9_]*_)SECRET(_[A-Z0-9_]*|\b)\s*=\s(\'|")[^\'"]+(\'|")')
     ),
 )
 
@@ -30,12 +41,17 @@ VAR_NAMES = (
 def check_line(line, check_list):
     messages = []
 
-    for key, msg, regexps in check_list:
+    for tup in check_list:
+        if len(tup) == 3:
+            key, msg, regexps = tup
+            cond = any
+        else:
+            key, msg, regexps, cond = tup
+
         if not isinstance(regexps, (list, tuple)):
             regexps = [regexps]
-        for regexp in regexps:
-            if regexp.search(line):
-                messages.append((key, msg))
+        if cond([regexp.search(line) for regexp in regexps]):
+            messages.append((key, msg))
 
     return messages
 
