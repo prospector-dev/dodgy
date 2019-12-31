467f44240030b52a337cd0cb721ddd65c4c0742c
Carl Crowder <carl@redcliff.ltd>
2019-12-31 14:39:24 +0100

    Making all tests/CI/classifiers match the supported python versions (3.4-3.8)

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ _classifiers = (
     'License :: OSI Approved :: MIT License',
     'Operating System :: Unix',
     'Topic :: Software Development :: Quality Assurance',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
